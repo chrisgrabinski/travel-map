a2b84cb28a821fdb68395a24ebb35e2675d7b525
Christopher Grabinski <christopher@unbabel.com>
2019-03-27 00:25:32 +0100

    Add functionality to add items to a list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [countryInput, setCountryInput] = useState("");
+
+  function addCountry(event) {
+    event.preventDefault();
+    setVisitedCountries([...visitedCountries, countryInput]);
   }
-}
+
+  return (
+    <div className="App">
+      <div>
+        <form action="" onSubmit={addCountry}>
+          <input
+            type="text"
+            value={countryInput}
+            onChange={event => setCountryInput(event.target.value)}
+          />
+          <button type="submit">Add country</button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {visitedCountries.map(visitedCountry => (
+            <li key={visitedCountry}>{visitedCountry}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
