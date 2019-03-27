61f2f7702fc5a98c4bdbe8d8e302ad74313c7b57
Christopher Grabinski <christopher@unbabel.com>
2019-03-27 09:27:17 +0100

    Updates

---
+++ precache-manifest.7bd249b6aabb82eda188e019f93245cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d289f33a1c11d95e2955",
+    "url": "/travel-map/static/css/main.c61c5c85.chunk.css"
+  },
+  {
+    "revision": "d289f33a1c11d95e2955",
+    "url": "/travel-map/static/js/main.962669d1.chunk.js"
+  },
+  {
+    "revision": "9e05957d5aed5a764119",
+    "url": "/travel-map/static/js/runtime~main.b94ffd48.js"
+  },
+  {
+    "revision": "885d878d822b71c7caca",
+    "url": "/travel-map/static/js/2.255afa92.chunk.js"
+  },
+  {
+    "revision": "380572bea37d976a398a2f9030c20a76",
+    "url": "/travel-map/index.html"
+  }
+];
