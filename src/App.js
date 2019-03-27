0de3f3825d10c586e72925d8035ac1e1f50b2c17
Christopher Grabinski <christopher@unbabel.com>
2019-03-27 11:36:54 +0100

    Zoom to region when country is added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const App = () => {
   const [region, setRegion] = useState("world");
 
   useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;flag")
+    fetch(
+      "https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;flag;region;subregion"
+    )
       .then(function(response) {
         return response.json();
       })
@@ -18,6 +20,66 @@ const App = () => {
         setCountriesList(myJson);
       });
   }, []);
+
+  function toRegionId(regionName) {
+    if (regionName === "Africa") {
+      return "002";
+    } else if (regionName === "Northern Africa") {
+      return "015";
+    } else if (regionName === "Western Africa") {
+      return "011";
+    } else if (regionName === "Middle Africa") {
+      return "017";
+    } else if (regionName === "Eastern Africa") {
+      return "014";
+    } else if (regionName === "Southern Africa") {
+      return "018";
+    } else if (regionName === "Europe") {
+      return "150";
+    } else if (regionName === "Northern Europe") {
+      return "154";
+    } else if (regionName === "Western Europe") {
+      return "155";
+    } else if (regionName === "Eastern Europe") {
+      return "151";
+    } else if (regionName === "Southern Europe") {
+      return "039";
+    } else if (regionName === "Americas") {
+      return "019";
+    } else if (regionName === "Northern America") {
+      return "021";
+    } else if (regionName === "Caribbean") {
+      return "029";
+    } else if (regionName === "Central America") {
+      return "013";
+    } else if (regionName === "South America") {
+      return "005";
+    } else if (regionName === "Asia") {
+      return "142";
+    } else if (regionName === "Central Asia") {
+      return "143";
+    } else if (regionName === "Eastern Asia") {
+      return "030";
+    } else if (regionName === "Southern Asia") {
+      return "034";
+    } else if (regionName === "South-Eastern Asia") {
+      return "035";
+    } else if (regionName === "Western Asia") {
+      return "145";
+    } else if (regionName === "Oceania") {
+      return "009";
+    } else if (regionName === "Australia and New Zealand") {
+      return "053";
+    } else if (regionName === "Melanesia") {
+      return "054";
+    } else if (regionName === "Micronesia") {
+      return "057";
+    } else if (regionName === "Polynesia") {
+      return "061";
+    }
+
+    return false;
+  }
 
   function testForCountryDuplicate(countryName) {
     if (visitedCountries.find(country => country.name === countryName)) {
@@ -37,8 +99,10 @@ const App = () => {
     if (testForCountryDuplicate(countryInput)) {
       setErrorMessage(`You've already added ${countryInput} to the list!`);
     } else {
+      const countryData = getCountryData(countryInput);
       setErrorMessage(null);
-      setVisitedCountries([...visitedCountries, getCountryData(countryInput)]);
+      setVisitedCountries([...visitedCountries, countryData]);
+      setRegion(toRegionId(countryData.subregion));
     }
 
     setCountryInput("");
@@ -118,6 +182,7 @@ const App = () => {
         <select
           name="region"
           id="region"
+          value={region}
           onChange={event => setRegion(event.target.value)}
           onBlur={event => setRegion(event.target.value)}
           className={css`
